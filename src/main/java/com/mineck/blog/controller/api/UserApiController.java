c539fa53e548ceef8252db68e454e84e66d7bd5a
LFOX <lmhfox3573@naver.com>
2022-11-24 13:32:06 +0900

    commit_2
    .

--- src/main/java/com/mineck/blog/controller/api/UserApiController.java
+++ src/main/java/com/mineck/blog/controller/api/UserApiController.java
@@ -1,12 +1,22 @@
 package com.mineck.blog.controller.api;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mineck.blog.config.auth.PrincipalDetail;
 import com.mineck.blog.dto.ResponseDto;
 import com.mineck.blog.model.RoleType;
 import com.mineck.blog.model.User;
@@ -17,11 +27,26 @@ public class UserApiController {
 	
 	@Autowired
 	private UserService userService;
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
 	
 	@PostMapping("/auth/joinProc")
 	public ResponseDto<Integer> save(@RequestBody User user) {
 		System.out.println("UserApiController: save 호출됨");
 		userService.회원가입(user);
+		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
+	}
+
+	@PutMapping("/user")
+	public ResponseDto<Integer> update(@RequestBody User user) {
+		System.out.println("UserApiController: update 호출됨");
+		userService.회원수정(user);
+		
+		//update session
+		Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
 	}
 	
--- src/main/java/com/mineck/blog/service/UserService.java
+++ src/main/java/com/mineck/blog/service/UserService.java
@@ -2,6 +2,10 @@ package com.mineck.blog.service;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,5 +30,16 @@ public class UserService {
 		user.setPassword((encPassword));
 		user.setRole(RoleType.USER);
 		userRepository.save(user);
+	}
+
+	@Transactional
+	public void 회원수정(User user) {
+		User persistance = userRepository.findById(user.getId()).orElseThrow(()->{
+			return new IllegalArgumentException("회원 찾기 실패");
+		});
+		String rawPassword = user.getPassword();
+		String encPassword = encoder.encode(rawPassword);
+		persistance.setPassword(encPassword);
+		persistance.setEmail(user.getEmail());
 	}
 }
